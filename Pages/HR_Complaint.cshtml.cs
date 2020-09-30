068f554cec0470c66bc6abcaf157788867de1075
Matt Copple <matt.copple@ktogaming.com>
2020-09-30 16:54:56 -0500

    Added Hardware Request Template Page Added HR Complaint Template Page Added In Progress Page Updated some feilds in SARF Changes Layout Options Added Banner for SARF Added In progress [pin for in progress page

---
+++ Pages/HR_Complaint.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Systems_Access_Request_Form.Pages
+{
+    public class HR_ComplaintModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
